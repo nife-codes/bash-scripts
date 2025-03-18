afe90ecbcb0e0411a435a53e6b6f7b3d36a0b3f3
zeemorayo <hephzibbahmorayo@gmail.com>
2025-03-18 15:06:36 +0100

    Added PowerShell script

---
+++ myscript.ps1
@@ -0,0 +1,4 @@
+Write-Output "Hello, $env:UserName!"
+Write-Output "Today's date is: $(Get-Date)"
+Write-Output "Currently logged-in users:"
+Get-WmiObject -Class Win32_ComputerSystem | Select-Object -ExpandProperty UserName
